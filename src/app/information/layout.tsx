9a3decbaad6ca16e6486d3aecc68aee2f6dc92e2
Daniela <danscarballo@gmail.com>
2023-07-09 22:12:50 -0300

    Importa fonts

--- src/app/information/layout.tsx
+++ src/app/information/layout.tsx
@@ -1,7 +1,14 @@
+import { firacode } from "../models";
 import cardLayoutStyles from "./information-layout.module.css";
 
 function CardLayout({ children }: { children: React.ReactNode }) {
-  return <div className={cardLayoutStyles.informationLayout}>{children}</div>;
+  return (
+    <div
+      className={`${firacode.className} ${cardLayoutStyles.informationLayout}`}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default CardLayout;
---
+++ src/app/models/index.ts
@@ -0,0 +1,1 @@
+export * from "./firacode.fonts";
